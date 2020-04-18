e498123a12f85c9fbbafce6ea98cf087c6bfa688
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-18 09:38:19 -0300

    Fix add created_at and updated_at in appointments

--- src/database/migrations/1587167572337-CreateAppointments.ts
+++ src/database/migrations/1587167572337-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1587167572337
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
