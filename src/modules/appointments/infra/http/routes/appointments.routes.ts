8d4d6bdae0034e94e14c52090724b1a829550375
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-05 15:43:00 -0300

    chore: Refactored with implement dependency inversion

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -7,7 +7,6 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -19,6 +18,8 @@ appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
+
+  const appointmentRepository = new AppointmentsRepository();
 
   const parsedDate = parseISO(date);
 
