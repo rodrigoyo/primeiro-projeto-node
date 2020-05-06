34548b7521fa64cb0aee6adf687176a86edc7ba2
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-06 11:42:06 -0300

    test: Two apointments in the same time and create simple appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +19,23 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('12312313');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointmentDate = new Date(2020, 4, 10, 11);
 
-  // })
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12312313',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12312313',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
