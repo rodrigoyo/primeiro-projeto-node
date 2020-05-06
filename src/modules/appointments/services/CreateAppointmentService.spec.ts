444c71488e86b43542f58e2d6a9ad92dc3779578
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-06 11:15:34 -0300

    chore: Created fake repository with appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test('sum to numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12312313',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12312313');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+
+  // })
 });
