89f0a4f0ff759c6e2dfbf26725b4c48392b596c0
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-08 16:46:27 -0300

    test: Should be able to reset password

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -20,6 +20,14 @@ class FakeUserTokensRepository implements IUsersTokensRepository {
 
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.usersTokens.find(
+      findToken => findToken.token === token,
+    );
+
+    return userToken;
+  }
 }
 
 export default FakeUserTokensRepository;
