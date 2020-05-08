9d27b375bbb7babc9fe41b719ca85991b226898a
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-08 15:56:08 -0300

    bug: Add resolve promise

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -31,7 +31,7 @@ describe('CreateAppointment', () => {
       provider_id: '12312313',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '12312313',
