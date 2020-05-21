cf5c72e2937e74586e4522d948b5856f9b8cb069
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-21 19:17:33 -0300

    chore: Add user id in appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -18,6 +18,7 @@ describe('CreateAppointment', () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '12312313',
+      user_id: '12312313',
     });
 
     expect(appointment).toHaveProperty('id');
@@ -30,12 +31,14 @@ describe('CreateAppointment', () => {
     await createAppointment.execute({
       date: appointmentDate,
       provider_id: '12312313',
+      user_id: '12312313',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '12312313',
+        user_id: '12312313',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
